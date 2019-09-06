d970a53f2ae34aa8a90b2b11b8c1559a3a413c77
taogu.mxx <taogu.mxx@taobao.com>
2019-09-06 20:28:44 +0800

    Polish Lesson 30

--- 2019.01.04「小马哥技术周报」- 第十五期《Apache Dubbo 微服务系列之 Dubbo 与 Spring Cloud 整合》/dubbo-samples/demo-api/src/main/java/mercyblitz/tech/weekly/dubbo/demo/api/CalculateService.java
+++ 2019.01.04「小马哥技术周报」- 第十五期《Apache Dubbo 微服务系列之 Dubbo 与 Spring Cloud 整合》/dubbo-samples/demo-api/src/main/java/mercyblitz/tech/weekly/dubbo/demo/api/CalculateService.java
@@ -1,7 +1,22 @@
 package mercyblitz.tech.weekly.dubbo.demo.api;
 
+import jdk.nashorn.internal.ir.annotations.Reference;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient("${abc.name}")
 public interface CalculateService {
 
+    @GetMapping("/")
     int divide(int a, int b);
 
 }
+
+class  A {
+
+//    @Reference
+    @Autowired
+    private CalculateService calculateService;
+}
+
---
+++ 2019.09.06 「小马哥技术周报」- 第三十期《Apache Dubbo 设计与实现系列之 Spring Cloud 工程（上）》/README.md
@@ -0,0 +1,6 @@
+# 「小马哥技术周报」- 第三十期  Apache Dubbo 设计与实现系列之 Dubbo Spring Cloud 工程（上）
+
+
+## 功能特性
+
+
